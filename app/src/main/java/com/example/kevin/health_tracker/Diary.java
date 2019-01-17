b0f01931e8d1a49fa31bcb5bd798bba2d0a0fbd2
Kevinrosales <kevinrosalesolvera@gmail.com>
2019-01-17 11:59:35 -0800

    need to populate db

--- app/src/main/java/com/example/kevin/health_tracker/Diary.java
+++ app/src/main/java/com/example/kevin/health_tracker/Diary.java
@@ -1,15 +1,14 @@
 package com.example.kevin.health_tracker;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 public class Diary extends AppCompatActivity {
@@ -22,33 +21,35 @@ public class Diary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_exercise);
-        displayExercises();
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "exercise").build();
+//        displayExercises();
     }
 
-    public void displayExercises() {
-        List<Exercise> exercises = db.exerciseDao().getAll();
-        DiaryNotes = findViewById(R.id.exerciseEntrys);
-        ArrayAdapter<Exercise> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, exercises);
-        DiaryNotes.setAdapter(arrayAdapter);
-    }
-
-    public void recordExercise(View v){
-         EditText titleInput = findViewById(R.id.addTitle);
-        String title = titleInput.getText().toString();
-
-         EditText quantityInput = findViewById(R.id.addQuantity);
-        String quantity = quantityInput.getText().toString();
-
-         EditText descriptionInput = findViewById(R.id.addDescription);
-        String description = descriptionInput.getText().toString();
-
-        EditText timeInput = findViewById(R.id.addTimeStamp);
-        String timeStamp = timeInput.getText().toString();
-
-        Exercise recordedExercise = new Exercise(title, quantity, description, timeStamp);
-        db.exerciseDao().insertAll(recordedExercise);
-        finish();
-        startActivity(getIntent());
-    }
+//    public void displayExercises() {
+//        List<Exercise> exercises = db.exerciseDao().getAll();
+//        DiaryNotes = findViewById(R.id.exerciseEntrys);
+//        ArrayAdapter<Exercise> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, exercises);
+//        DiaryNotes.setAdapter(arrayAdapter);
+//    }
+//
+//    public void recordExercise(View v){
+//        EditText titleInput = findViewById(R.id.addTitle);
+//        String title = titleInput.getText().toString();
+//
+//        EditText quantityInput = findViewById(R.id.addQuantity);
+//        String quantity = quantityInput.getText().toString();
+//
+//        EditText descriptionInput = findViewById(R.id.addDescription);
+//        String description = descriptionInput.getText().toString();
+//
+//        EditText timeInput = findViewById(R.id.addTimeStamp);
+//        String timeStamp = timeInput.getText().toString();
+//
+//        Exercise recordedExercise = new Exercise(title, quantity, description, timeStamp);
+//        db.exerciseDao().insertAll(recordedExercise);
+//        finish();
+//        startActivity(getIntent());
+//    }
 
 }
--- app/src/main/java/com/example/kevin/health_tracker/NotificationReceiver.java
+++ app/src/main/java/com/example/kevin/health_tracker/NotificationReceiver.java
@@ -5,7 +5,7 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationManagerCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 // got this code from (https://gist.github.com/BrandonSmith/6679223)
 public class NotificationReceiver extends BroadcastReceiver {
