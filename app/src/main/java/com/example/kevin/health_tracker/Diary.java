d0a6362775eded161e1499605ca0cc967ce35b77
Kevinrosales <kevinrosalesolvera@gmail.com>
2019-01-23 13:28:10 -0800

    getting data from server

--- app/src/main/java/com/example/kevin/health_tracker/Diary.java
+++ app/src/main/java/com/example/kevin/health_tracker/Diary.java
@@ -3,12 +3,21 @@ package com.example.kevin.health_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import com.google.gson.Gson;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.kevin.health_tracker.Database.AppDatabase;
 
 import java.text.DateFormat;
@@ -27,6 +36,7 @@ public class Diary extends AppCompatActivity {
         setContentView(R.layout.activity_save_exercise);
         db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "exercise").allowMainThreadQueries().fallbackToDestructiveMigration().build();
         displayExercises();
+        getBackendData();
     }
 // I worked with Amy and Sooz with some help from nick Crain as well
     public void displayExercises() {
@@ -56,6 +66,36 @@ public class Diary extends AppCompatActivity {
         Exercise recordedExercise = new Exercise(title, quantity, description, timeStamp);
         db.exerciseDao().insertAll(recordedExercise);
         startActivity(getIntent());
+    }
+
+//////////////////////////////backend server things////////////////////////////////
+// got this code from ()
+    public void getBackendData() {
+
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://kevin-health-tracker.herokuapp.com/exercises";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response", response);
+                        Gson gson =  new Gson();
+                        Exercise[] exercises = gson.fromJson(response, Exercise[].class);
+                        Log.d("Exercises", String.valueOf(exercises));
+                        db.exerciseDao().insertAll(exercises);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Response error", String.valueOf(error));
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
 }
